5b1d6a69fba72c5d22ddbe6e8176303c6b70c2e0
Rafael Gonçalves <rafaelswitek@gmail.com>
2025-11-30 17:44:08 -0300

    Adiciona SafeAreaProvider e SafeAreaView para melhor gerenciamento de áreas seguras nas telas

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import * as React from "react";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import HomeScreen from "./src/screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context"; 
 
-const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <HomeScreen />
-      </NavigationContainer>
-    </PaperProvider>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <HomeScreen />
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,38 +1,41 @@
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import MediaTabs from "../components/MediaTabs";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function HomeScreen() {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarStyle: { backgroundColor: "#ffffff" },
-        tabBarIndicatorStyle: { backgroundColor: "#6200ee" },
-        tabBarLabelStyle: { fontWeight: "bold" },
-        lazy: true,
-      }}
-    >
-      <Tab.Screen name="Filmes">
-        {() => <MediaTabs mediaType="movies" />}
-      </Tab.Screen>
+    <SafeAreaView style={{ flex: 1 }}>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: { backgroundColor: "#ffffff" },
+          tabBarIndicatorStyle: { backgroundColor: "#6200ee" },
+          tabBarLabelStyle: { fontWeight: "bold" },
+          lazy: true,
+        }}
+      >
+        <Tab.Screen name="Filmes">
+          {() => <MediaTabs mediaType="movies" />}
+        </Tab.Screen>
 
-      <Tab.Screen name="Séries">
-        {() => <MediaTabs mediaType="tv" />}
-      </Tab.Screen>
+        <Tab.Screen name="Séries">
+          {() => <MediaTabs mediaType="tv" />}
+        </Tab.Screen>
 
-      <Tab.Screen name="Jogos">
-        {() => <MediaTabs mediaType="games" />}
-      </Tab.Screen>
+        <Tab.Screen name="Jogos">
+          {() => <MediaTabs mediaType="games" />}
+        </Tab.Screen>
 
-      <Tab.Screen name="Livros">
-        {() => <MediaTabs mediaType="books" />}
-      </Tab.Screen>
+        <Tab.Screen name="Livros">
+          {() => <MediaTabs mediaType="books" />}
+        </Tab.Screen>
 
-      <Tab.Screen name="Outros">
-        {() => <MediaTabs mediaType="others" />}
-      </Tab.Screen>
-    </Tab.Navigator>
+        <Tab.Screen name="Outros">
+          {() => <MediaTabs mediaType="others" />}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </SafeAreaView>
   );
 }
